d5623a1e114bfa006e19533dd63e82c5b73f0040
David <dlennie85@gmail.com>
2026-05-17 23:23:30 +0100

    Adjust receipt logo crop

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1102,7 +1102,7 @@ export default function CheckoutPage() {
             .center { text-align: center; }
             .logo-wrap {
               width: 62mm;
-              height: 24mm;
+              height: 30mm;
               margin: 0 auto 2px;
               overflow: hidden;
               display: flex;
